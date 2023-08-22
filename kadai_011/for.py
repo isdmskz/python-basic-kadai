1ef4d538b38b23c564b71ae6973e4ff8c8fd56b0
isdmskz <137642268+isdmskz@users.noreply.github.com>
2023-08-22 15:20:16 +0900

    kadai_011フォルダをアップロード

---
+++ kadai_011/for.py
@@ -0,0 +1,5 @@
+print('for文を使った出力結果')
+list = ["水","金","地","火","木","土","天","海","冥"]
+
+for planet in list:
+  print(planet)
---
+++ kadai_011/while.py
@@ -0,0 +1,9 @@
+print('while文を使った出力結果')
+list = ["水","金","地","火","木","土","天","海","冥"]
+
+i = 0
+
+while i < 9:
+  planet = list[i]
+  print(planet)
+  i = i + 1
